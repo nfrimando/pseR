6cafad7b59fc6edbc98c7dd2bb2c7c2395c2c3eb
Nigel Rimando <nigel.rimando@firstcircle.com>
2020-04-10 02:43:04 +0800

    changing imports to depends

--- R/pull_pse_data.R
+++ R/pull_pse_data.R
@@ -4,52 +4,63 @@
 #' values from a source. Currently only pulls from Investagrams and limited
 #' to past one year data.
 #'
-#' @param code Stock Code from PSE (e.g. JFC, FMETF, BPI)
+#' @param code Vector of Stock Code(s) from PSE (e.g. JFC, FMETF, BPI)
 #' @return A dataframe with available historical stock data
 #' 
 #' @import dplyr rvest
 #' @importFrom stringr str_replace_all
 #' @importFrom glue glue
 #' @importFrom assertthat assert_that
+#' @importFrom purrr map_df
 #' @export
 #' 
 #' @examples 
 #' pull_pse_data("JFC")
 pull_pse_data <- function(code) {
   
-  url <- glue("https://www.investagrams.com/Stock/{code}")
-  
-  # Extract Data From Investagrams Historical Table
-  html_content <- url %>%
-    read_html() %>%
-    html_nodes(xpath = '//*[@id="HistoricalDataTable"]') %>%
-    html_table()
-  
-  assert_that(
-    length(html_content) != 0,
-    msg = "No Data Extracted. Please Check if Stock Code is Valid"
+  all_historical.dt <- map_df(
+    code,
+    function(code) {
+      
+      message(glue("Pulling data for {code}"))
+      
+      url <- glue("https://www.investagrams.com/Stock/{code}")
+      
+      # Extract Data From Investagrams Historical Table
+      html_content <- url %>%
+        read_html() %>%
+        html_nodes(xpath = '//*[@id="HistoricalDataTable"]') %>%
+        html_table()
+      
+      assert_that(
+        length(html_content) != 0,
+        msg = "No Data Extracted. Please Check if Stock Code is Valid"
+      )
+      
+      historical.dt.raw <- html_content[[1]]
+      
+      # Use First Row as Column Names
+      colnames(historical.dt.raw) <- historical.dt.raw[1,]
+      
+      # Apply Transformations
+      historical.dt <- historical.dt.raw[-c(1, nrow(historical.dt.raw)),] %>% 
+        transmute(
+          code = code,
+          date = format.Date(strptime(Date, "%b %d, %Y"), '%Y-%m-%d'),
+          close = as.numeric(`Last Price`),
+          change = `Change`,
+          open = `Open`,
+          low = `Low`,
+          high = `High`
+        ) %>% 
+        mutate_at(
+          c("change", "open", "low", "high"),
+          as.numeric
+        )
+      
+      return(historical.dt)
+    }
   )
-   
-  historical.dt.raw <- html_content[[1]]
-  
-  # Use First Row as Column Names
-  colnames(historical.dt.raw) <- historical.dt.raw[1,]
-  
-  # Apply Transformations
-  historical.dt <- historical.dt.raw[-c(1, nrow(historical.dt.raw)),] %>% 
-    transmute(
-      code = code,
-      date = format.Date(strptime(Date, "%b %d, %Y"), '%Y-%m-%d'),
-      close = as.numeric(`Last Price`),
-      change = `Change`,
-      open = `Open`,
-      low = `Low`,
-      high = `High`
-    ) %>% 
-    mutate_at(
-      c("change", "open", "low", "high"),
-      as.numeric
-    )
-  
-  return(historical.dt)
+
+  return(all_historical.dt)
 }
--- README.md
+++
@@ -1,3 +0,0 @@
-# pseR
-
-Pull and analyse PSE data. Currently limited only to Investagrams as source which is limited to past year data only.
