8503c0cfd86c02a2f3455070cfa96c569d6bb354
Nigel Rimando <nigel.rimando@firstcircle.com>
2020-04-18 15:40:12 +0800

    scraping bands

--- R/scrape_investagram.R
+++ R/scrape_investagram.R
@@ -4,7 +4,8 @@
 #' returns list of details
 #'
 #' @param code Vector of Stock Code(s) from PSE (e.g. JFC, FMETF, BPI)
-#' @param details Vector containing details to scrape. `hist` - dataframe of historical data
+#' @param details Vector containing details to scrape. `hist` - dataframe of historical data. 
+#' `bands` - basic analytics
 #' @return A list with investagram information
 #' 
 #' @import dplyr rvest
@@ -13,7 +14,7 @@
 #' 
 #' @examples 
 #' scrape_investagram(c("JFC", "GLO"))
-scrape_investagram <- function(code, details = c("hist")) {
+scrape_investagram <- function(code, details = c("hist", "bands")) {
   
   stock_information.list <- list()
   
@@ -23,19 +24,44 @@ scrape_investagram <- function(code, details = c("hist")) {
       
       url <- glue("https://www.investagrams.com/Stock/{i}")
       
-      # Historical Table -------------------
+      html_content <- url %>%
+        read_html()
+      
+      # Historical Table ----------------------------------
       if ("hist" %in% details) {
-        html_content <- url %>%
-          read_html() %>%
-          html_nodes(xpath = '//*[@id="HistoricalDataTable"]') %>%
+        data <- html_content %>% 
+          html_nodes(xpath = '//*[@id="HistoricalDataTable"]') %>% 
           html_table()
         
         assert_that(
-          length(html_content) != 0,
-          msg = "No Data Extracted. Please Check if Stock Code is Valid"
+          length(data) != 0,
+          msg = "No Historical Data Extracted. Please Check if Stock Code is Valid"
         )
         
-        stock_information.list[[i]]$historical_data <- html_content[[1]]
+        stock_information.list[[i]]$historical_data <- data[[1]]
+      }
+      
+      # Support Table ----------------------------------
+      if ("bands" %in% details) {
+        data <- html_content %>% 
+          html_nodes('.stock-information-table') %>%
+          html_table(fill = TRUE) %>% 
+          {.[[5]]} %>% {
+            list(
+              support_1 = .$X2[which(.$X1 == "Support 1:")],
+              support_2 = .$X2[which(.$X1 == "Support 2:")],
+              resist_1 = .$X4[which(.$X3 == "Resistance 1:")],
+              resist_2 = .$X4[which(.$X3 == "Resistance 2:")]
+            ) %>% 
+              lapply(function(x) {str_replace_all(x,",","") %>% as.numeric()})
+          }
+        
+        assert_that(
+          length(data) != 0,
+          msg = "No Bands Data Extracted. Please Check if Stock Code is Valid"
+        )
+        
+        stock_information.list[[i]]$bands <- data
       }
   
   }
