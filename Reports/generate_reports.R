2dc875ca03f39f6d3dc5a10c82e1aebefcc5ef3e
Nigel Rimando <nigel.rimando@firstcircle.com>
2020-04-19 00:39:25 +0800

    adding date to filename

--- Reports/generate_reports.R
+++ Reports/generate_reports.R
@@ -12,6 +12,6 @@ for (type in c("Blue Chip", "Alternative", "Concept Play", "My Portfolio")) {
     params = list(
       stock_type = type 
     ),
-    output_file = paste0("bands_",make_clean_names(type),".html")
+    output_file = paste0(str_replace_all(Sys.Date(), "-", ""), "bands_",make_clean_names(type),".html")
   )  
 }
