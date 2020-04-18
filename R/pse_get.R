be71f583ecabd41722bcdd3c21bcf7af6a0f935d
Nigel Rimando <nigel.rimando@firstcircle.com>
2020-04-18 20:32:22 +0800

    editing scrape_investagram;

--- R/pse_get.R
+++ R/pse_get.R
@@ -4,7 +4,7 @@
 #' values from a source. Currently only pulls from Investagrams and limited
 #' to past one year data.
 #'
-#' @param code Vector of Stock Code(s) from PSE (e.g. JFC, FMETF, BPI)
+#' @param codes Vector of Stock Code(s) from PSE (e.g. JFC, FMETF, BPI)
 #' @return A dataframe with available historical stock data
 #' 
 #' @import dplyr rvest
@@ -19,38 +19,17 @@
 #' 
 #' @examples 
 #' pse_get("JFC")
-pse_get <- function(code) {
+pse_get <- function(codes) {
 
-  stock_info.list <- scrape_investagram(code)
+  stock_info.list <- scrape_investagram(codes)
     
   # Combining into one dataframe
   all_historical.dt <- map_df(
     stock_info.list,
     function(stock_info) {
-      
-      historical.dt.raw <- stock_info$historical_data
-      
-      # Use First Row as Column Names
-      colnames(historical.dt.raw) <- historical.dt.raw[1,]
-      
-      # Apply Transformations
-      historical.dt <- historical.dt.raw[-c(1, nrow(historical.dt.raw)),] %>% 
-        transmute(
-          code = code,
-          date = format.Date(strptime(Date, "%b %d, %Y"), '%Y-%m-%d'),
-          close = str_replace_all(`Last Price`, ",", ""),
-          change = str_replace_all(`Change`, ",", ""),
-          open = str_replace_all(`Open`, ",", ""),
-          low = str_replace_all(`Low`, ",", ""),
-          high = str_replace_all(`High`, ",", "")
-        ) %>% 
-        mutate_at(
-          c("close", "change", "open", "low", "high"),
-          as.numeric
-        )
-      
-      return(historical.dt)
-    }
+      stock_info$data
+    },
+    .id = "code"
   )
   
   return(all_historical.dt)
--- R/scrape_investagram.R
+++ R/scrape_investagram.R
@@ -1,11 +1,11 @@
-#' Scrape Investagram Stock Info
+#' Scrape Investagram Stock Info (Individual)
 #'
 #' This function extracts raw HTML from Investagram UI and 
-#' returns list of details
+#' returns list of details for one stock
 #'
-#' @param code Vector of Stock Code(s) from PSE (e.g. JFC, FMETF, BPI)
+#' @param code single stock code
 #' @param details Vector containing details to scrape. `hist` - dataframe of historical data. 
-#' `bands` - basic analytics
+#' `bands` - basic bands
 #' @return A list with investagram information
 #' 
 #' @import dplyr rvest
@@ -13,58 +13,99 @@
 #' @importFrom assertthat assert_that
 #' 
 #' @examples 
-#' scrape_investagram(c("JFC", "GLO"))
-scrape_investagram <- function(code, details = c("hist", "bands")) {
+#' scrape_investagram_indiv("JFC")
+scrape_investagram_indiv <- function(code, details = c("hist", "bands")) {
   
-  stock_information.list <- list()
+  assert_that(length(code) == 1, msg = "Enter only one stock code")
+  assert_that(all(details %in% c("hist", "bands")), msg = "Enter valid details")
   
-  for (i in code) {
-      
-      message(glue("Pulling data for {i}"))
-      
-      url <- glue("https://www.investagrams.com/Stock/{i}")
-      
-      html_content <- url %>%
-        read_html()
-      
-      # Historical Table ----------------------------------
-      if ("hist" %in% details) {
-        data <- html_content %>% 
-          html_nodes(xpath = '//*[@id="HistoricalDataTable"]') %>% 
-          html_table()
-        
-        assert_that(
-          length(data) != 0,
-          msg = "No Historical Data Extracted. Please Check if Stock Code is Valid"
-        )
-        
-        stock_information.list[[i]]$historical_data <- data[[1]]
-      }
-      
-      # Support Table ----------------------------------
-      if ("bands" %in% details) {
-        data <- html_content %>% 
-          html_nodes('.stock-information-table') %>%
-          html_table(fill = TRUE) %>% 
-          {.[[5]]} %>% {
-            list(
-              support_1 = .$X2[which(.$X1 == "Support 1:")],
-              support_2 = .$X2[which(.$X1 == "Support 2:")],
-              resist_1 = .$X4[which(.$X3 == "Resistance 1:")],
-              resist_2 = .$X4[which(.$X3 == "Resistance 2:")]
-            ) %>% 
-              lapply(function(x) {str_replace_all(x,",","") %>% as.numeric()})
-          }
-        
-        assert_that(
-          length(data) != 0,
-          msg = "No Bands Data Extracted. Please Check if Stock Code is Valid"
-        )
-        
-        stock_information.list[[i]]$bands <- data
-      }
+  info.list <- list()
   
+  message(glue("Pulling data for {code}"))
+  
+  url <- glue("https://www.investagrams.com/Stock/{code}")
+  
+  html_content <- url %>%
+    read_html()
+  
+  # Historical Table ----------------------------------
+  if ("hist" %in% details) {
+    historical_raw.dt <- html_content %>% 
+      html_nodes(xpath = '//*[@id="HistoricalDataTable"]') %>% 
+      html_table() %>% 
+      {.[[1]]}
+    
+    # Use First Row as Column Names
+    colnames(historical_raw.dt) <- historical_raw.dt[1,]
+    
+    # Transmute Info
+    historical.dt <- historical_raw.dt[-c(1, nrow(historical_raw.dt)),] %>% 
+      transmute(
+        date = format.Date(strptime(Date, "%b %d, %Y"), '%Y-%m-%d'),
+        close = str_replace_all(`Last Price`, ",", ""),
+        change = str_replace_all(`Change`, ",", ""),
+        open = str_replace_all(`Open`, ",", ""),
+        low = str_replace_all(`Low`, ",", ""),
+        high = str_replace_all(`High`, ",", "")
+      ) %>% 
+      mutate_at(
+        c("close", "change", "open", "low", "high"),
+        as.numeric
+      )
+    
+    assert_that(
+      length(historical.dt) != 0,
+      msg = "No Historical Data Extracted. Please Check if Stock Code is Valid"
+    )
+    
+    info.list$data <- historical.dt
   }
   
-  return(stock_information.list)
+  # Support and Resist Info ----------------------------------
+  if ("bands" %in% details) {
+    bands.list <- html_content %>% 
+      html_nodes('.stock-information-table') %>%
+      html_table(fill = TRUE) %>% 
+      {.[[5]]} %>% {
+        list(
+          support_1 = .$X2[which(.$X1 == "Support 1:")],
+          support_2 = .$X2[which(.$X1 == "Support 2:")],
+          resist_1 = .$X4[which(.$X3 == "Resistance 1:")],
+          resist_2 = .$X4[which(.$X3 == "Resistance 2:")]
+        ) %>% 
+          lapply(function(x) {str_replace_all(x,",","") %>% as.numeric()})
+      }
+    
+    assert_that(
+      length(bands.list) != 0,
+      msg = "No Bands Data Extracted. Please Check if Stock Code is Valid"
+    )
+    
+    info.list$bands <- bands.list
+  }
+  
+  return(
+    info.list
+  )
+}
+
+#' Scrape Investagram Stock Info
+#'
+#' This function extracts raw HTML from Investagram UI and 
+#' returns list of details
+#'
+#' @param codes Vector of Stock Code(s) from PSE (e.g. JFC, FMETF, BPI)
+#' @param details Vector containing details to scrape. `hist` - dataframe of historical data. 
+#' `bands` - basic analytics
+#' @return A list with investagram information
+#' 
+#' @importFrom assertthat assert_that
+#' 
+#' @examples 
+#' scrape_investagram(codes = c("JFC", "GLO"))
+scrape_investagram <- function(codes, details = c("hist", "bands")) {
+  Map(
+    function(x) {scrape_investagram_indiv(x, details)},
+    codes
+  )
 }
